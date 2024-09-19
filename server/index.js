43c5460efd5efd4f9a1534a8ba73facc3733c2a0
Ivan <ivan03ds@gmail.com>
2024-09-19 18:18:25 +0200

    Create react and test server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-// prova di js
+// Test fatto per avviare docker sulle macchine di laboratorio
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'html', 'create.html'))
+})
+
+app.listen(8000, () => {
+  console.log('Server online on port 8000')
+})
