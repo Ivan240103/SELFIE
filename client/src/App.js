5752ff6e026f39618e794a5d5798587d9cfc29d0
nicotambo03 <nic.tambini@gmail.com>
2024-10-01 10:40:29 +0200

    Inizio calendario

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Accesso from './components/Accesso'
+import Aprile from './components/Mesi/Aprile';  
+
+
+function App() {
+  return (
+    <div className="App">
+      <Aprile />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+function Home(){  //calendario
+
+    return (
+        
+    )
+}
+
+export default Home;
