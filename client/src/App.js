3873686e30b2db5adea37347cd8eae226b51fcbe
nicotambo03 <nic.tambini@gmail.com>
2024-12-20 15:00:59 +0100

    modifiche nome

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Access from './components/Access';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Calendar from './components/Calendar';
-import Tomato from './components/Tomato/Tomato';
+import Tomato from './components/Tomato/tomato';
 import Notes from './components/Notes'
 import Registration from './components/Registration';
 import Dashboard from './components/Dashboard';
@@ -16,7 +16,7 @@ function App() {
                 <Route path="/login" element={<Access />} />
                 <Route path="/register" element={<Registration />} />
                 <Route path="/Calendar" element={<Calendar />} />
-                <Route path="/Tomato" element={<Tomato />} />
+                <Route path="/tomato" element={<Tomato />} />
                 <Route path="/Notes" element={<Notes />} />
                 <Route path="/dashboard" element={<Dashboard/>} />
             </Routes>
