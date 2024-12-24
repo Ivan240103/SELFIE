e59d33f08bd75e6031644b57f1c88e268fc2cecc
Ivan240103 <ivan03ds@gmail.com>
2024-12-24 19:13:04 +0100

    refactor dashboard

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,73 +1,128 @@
-import { Link } from "react-router-dom";
-import "../css/Dashboard.css";
+import React, { useState, useEffect } from 'react'
+import { useNavigate, Link } from "react-router-dom";
+import axios from 'axios';
+import TimeMachine from './TimeMachine/TimeMachine'
+
 import calendarIcon from "../images/calendar-icon.png";
-import notes from "../images/notebook-pen-icon.png";
-import tomato from "../images/speed-icon.png";
-import logout from "../images/door-check-out-icon.png";
+import notesIcon from "../images/notebook-pen-icon.png";
+import tomatoIcon from "../images/speed-icon.png";
+import logoutIcon from "../images/door-check-out-icon.png";
+// TODO: servono icone per profilo e task
 
-function Dashboard(){
+import "../css/Dashboard.css";
 
-    return(
-        <div className="cen">
-            <div className="card">
-                <div className="figure">
-                    <h4><h2>Calendar</h2></h4>
-                    <p>Organize your routine!!</p>
-                    <img 
-                        src={calendarIcon} alt="Calendar"
-                        style={{ width: '100px', height: '100px' }} 
-                    />
-                </div>
-                <Link to="/Calendar">
-                    <button className="down"><b>Click here</b></button>
-                </Link>
-            </div> 
-            <div className="card">
-                <div className="figure">
-                    <h4><h2>Notes</h2></h4>
-                    <p>Write any thoughts!!</p>
-                    <img 
-                        src={notes} alt="Notes"
-                        style={{ width: '100px', height: '100px' }} 
-                    />
-                </div>
-                <Link to="/Notes">   
-                    <button className="down"><b>Click here</b></button>
-                </Link>
-            </div> 
-            <div className="card">
-                <div className="figure">
-                    <h4><h2>Tomato</h2></h4>
-                    <p>Set your time to study!!</p>
-                    <img 
-                        src={tomato} alt="Tomato"
-                        style={{ width: '100px', height: '100px' }} 
-                    />
-                </div>
-                <Link to="/Tomato">
-                    <button className="down"><b>Click here</b></button>
-                </Link>
-            </div> 
-            <div className="card">
-                <div className="figure">
-                    <h4><h2>Logout</h2></h4>
-                    <p>Do you want to login with another account?</p>
-                    <img 
-                        src={logout} alt="Logout"
-                        style={{ width: '100px', height: '100px' }} 
-                    />
-                </div>
-                <Link to="/login">
-                    <button
-                        className="down"
-                        onClick={() => localStorage.removeItem('token')}
-                    >
-                        <b>Click here</b>
-                    </button>
-                </Link>
-            </div> 
-        </div>
-    );
+function Dashboard() {
+  const navigate = useNavigate()
+  const [user, setUser] = useState({})
+
+  // verifica delle credenziali
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_API}/api/users/`, {
+          headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+        })
+        setUser(response.data)
+      } catch (error) {
+        console.error('Error fetching user')
+        navigate('/login')
+      }
+    }
+
+    fetchUser()
+  }, [navigate])
+
+  return(
+    <div>
+      <TimeMachine />
+      <div className='dash-container'>
+        <Link to='/profile'>
+          <div className='dash-card' id='dash-profile'>
+            <div className='dash-card-header'>
+              <img
+                // src={profileIcon}
+                alt='icona del profilo' />
+              <h2>Profilo personale</h2>
+              <p>Personalizza il tuo profilo!</p>
+            </div>
+            <div className='dash-card-preview'>
+              {/* preview profilo (qualche info: nome e cognome? email?) */}
+            </div>
+          </div>
+        </Link>
+        <Link to='/Calendar'>
+          <div className='dash-card' id='dash-calendar'>
+            <div className='dash-card-header'>
+              <img
+                src={calendarIcon}
+                alt='icona del calendario' />
+              <h2>Calendario</h2>
+              <p>Organizza la tua routine!</p>
+            </div>
+            <div className='dash-card-preview'>
+              {/* preview calendario (vista settimanale o giornaliera) */}
+            </div>
+          </div>
+        </Link>
+        <Link to='/Notes'>
+          <div className='dash-card' id='dash-note'>
+            <div className='dash-card-header'>
+              <img
+                src={notesIcon}
+                alt='icona delle note' />
+              <h2>Note</h2>
+              <p>Scrivi qualunque appunto!</p>
+            </div>
+            <div className='dash-card-preview'>
+              {/* preview ultima nota modificata */}
+            </div>
+          </div>
+        </Link>
+        <Link to='/task'>
+          <div className='dash-card' id='dash-task'>
+            <div className='dash-card-header'>
+              <img
+                // src={taskIcon}
+                alt='icona dei task' />
+              <h2>Task</h2>
+              <p>Traccia le cose da fare!</p>
+            </div>
+            <div className='dash-card-preview'>
+              {/* preview 3-5 task a scadenza più vicina */}
+            </div>
+          </div>
+        </Link>
+        <Link to='/tomato'>
+          <div className='dash-card' id='dash-tomato'>
+            <div className='dash-card-header'>
+              <img
+                src={tomatoIcon}
+                alt='icona del timer' />
+              <h2>Pomodoro</h2>
+              <p>Imposta il tempo di studio!</p>
+            </div>
+            <div className='dash-card-preview'>
+              {/* preview ultima sessione avviata */}
+            </div>
+          </div>
+        </Link>
+        <Link onClick={() => {
+          localStorage.removeItem('token')
+          navigate('/login')
+        }}>
+          <div className='dash-card' id='dash-logout'>
+            <div className='dash-card-header'>
+              <img
+                src={logoutIcon}
+                alt='icona del logout' />
+              <h2>Logout</h2>
+              <p>Esci dal profilo!</p>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
